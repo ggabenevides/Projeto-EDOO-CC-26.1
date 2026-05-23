4253976e8f1a3b259aa9297ae35bcb9908a7868a
sofiaavallone <sofia.sakovitz@gmail.com>
2026-05-23 16:45:38 -0300

    implementar lógica de finalização do jogo e dano

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "player.hpp"
 #include "coordenada.hpp"
 #include "coletavel.hpp"
+#include "inimigo.hpp"
 #include <iostream>
 
 int main(){
@@ -13,14 +14,20 @@ int main(){
     Mapa mapa;
     Player jogador(mapa);
 
-    // instanciando coletáveis e armazenando eles num vetor para poder avaliar 
+    // instanciando coletáveis e armazenando eles num vetor para poder avaliar
     srand(time(0)); // é usado no construtor
-    int coletavelCount = 0; 
+    int coletavelCount = 0;
     while (coletavelCount < 3)
     {
         Coletavel item(mapa);
         coletavelCount++;
     }
+
+    // instanciando inimigo
+    Inimigo inimigo(mapa);
+
+    // estado inicial
+    std::cout << mapa;
 
     // fluxo de jogo
     while(not terminou)
@@ -30,6 +37,10 @@ int main(){
         std::cout << "Para onde você deseja se movimentar? (w/a/s/d) ";
         std::cin >> movimento;
         jogador.movimentoWASD(mapa, movimento);
+        if (jogador.getColetaveisQtde() == 3) terminou = true;
+        if (inimigo.mover(mapa, jogador.getPosicao(), mapa.getMapaAtual()))
+            jogador.tomarDano();
+        if (jogador.getVida() == 0) terminou = true;
         std::cout << mapa;
     }
 
